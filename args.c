df024b2e60cda76948416d71fb34431e953fab78
Samuel Ohiri <84963762+therealsammy@users.noreply.github.com>
2022-10-17 22:52:35 +0100

    Create args.c

---
+++ args.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    // Write C code here
+    
+    printf("%d\n", argc - 1);
+
+    return 0;
+}
