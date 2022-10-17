601d785d533cbfbfe263f83a4eb4e4d5dc4a5117
Samuel Ohiri <84963762+therealsammy@users.noreply.github.com>
2022-10-17 22:39:44 +0100

    Create whatsmyname.c

---
+++ whatsmyname.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    // Write C code here
+    
+    printf("This is the program name: %s\n", argv[0]);
+
+    return 0;
+}
